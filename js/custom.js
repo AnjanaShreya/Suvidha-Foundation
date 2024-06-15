a9c355e2202c7e83b1e54798784766d355852cb7
AnjanaShreya <chslash01@yahoo.com>
2024-06-15 19:01:25 +0530

    commit

--- js/custom.js
+++ js/custom.js
@@ -10,9 +10,9 @@
 
     // BACKSTRETCH SLIDESHOW
     $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
+      "simages/Suvidha-s1.jpg", 
+      "simages/SuvidhaEvents.jpg",
+      "simages/Women'sDay.jpg"
     ],  {duration: 2000, fade: 750});
     
     // CUSTOM LINK
